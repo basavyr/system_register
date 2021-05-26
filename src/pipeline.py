97045e9cd4489fc0e812615dbe7fdd919239df3b
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-26 08:34:27 +0300

    add reagister read/write

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,4 +5,13 @@ reg = register.Register
 
 
 if __name__ == '__main__':
-    reg.Create_Register_Directory('dir')
+
+    x = reg.Create_Process_Register('bash', 'test')
+    y = reg.Read_Process_Register('bash')
+
+    try:
+        assert y[0] == 1, 'Error ocurred'
+    except AssertionError:
+        print('Errors ocurred')
+    else:
+        print('It works')
--- src/register.py
+++ src/register.py
@@ -2,8 +2,9 @@ import os
 
 
 class Utils:
-    def Create_Output_File(process):
+    def Register_File(process):
         file_name = f'{process}_instances.list'
+        return file_name
 
 
 class Register:
@@ -17,7 +18,7 @@ class Register:
 
     @staticmethod
     def Read_Process_Register(process):
-        process_file_name = Utils.Create_Output_File(process)
+        process_file_name = Utils.Register_File(process)
 
         # read content from file
         try:
@@ -27,8 +28,21 @@ class Register:
             instances = ' '
 
         if(instances == ' '):
+            return -1, instances
+        return 1, instances
+
+    @staticmethod
+    def Create_Process_Register(process, process_output):
+        process_file_name = Utils.Register_File(process)
+
+        # writes the output to a file
+        # only write to file if no error occurs
+        try:
+            with open(process_file_name, 'w+') as writer:
+                writer.write(process_output)
+        except OSError:
             return -1
-        return f'{len(instances)} -> {instances}'
+        return 1
 
 
 if __name__ == '__main__':
