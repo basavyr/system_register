6cc1a5f7f8ff2068824e860d4abe157d6e8a62a2
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-26 14:40:23 +0300

    adds skeleton for monitor method

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,24 +6,25 @@ tools = register.Utils
 watch = register.Monitoring
 
 
+def Execute_Process_Monitor(execution_time, process_list):
+    return 1, 1
+
+
 if __name__ == '__main__':
 
     process_list_file_name = 'PROCESSES'
 
+    # total execution time of the monitoring process
+    execution_time = 10
+
     watch.Create_External_Process_List(process_list_file_name)
     process_list = watch.Get_Processes_From_Process_List(
         process_list_file_name)
-    print(process_list)
-    # watch.Purge_External_Process_List(process_list_file_name)
-#
-    # x = reg.Create_Process_Register(process, 'test')
-    # y = reg.Read_Process_Register(process)
-#
-    # try:
-    # assert y[0] == 1, 'Error ocurred'
-    # except AssertionError:
-    # print('Errors ocurred')
-    # else:
-    # print('It works')
-    # print('Performing registry cleanup...')
-    # reg.Clean_Process_Registers(process)
+
+    Execute_Process_Monitor(execution_time, process_list)
+
+    # wipe out the process file at the end of the program execution
+    # optional command
+    purge_proc_list = False
+    if(purge_proc_list == True):
+        watch.Purge_External_Process_List(process_list_file_name)
