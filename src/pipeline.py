6ece6b1f90ec719c289d22d67aabdf24f58a2fef
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-27 21:13:29 +0300

    improves the pipeline main execution function

--- src/pipeline.py
+++ src/pipeline.py
@@ -23,20 +23,24 @@ def Execute_Process_Monitor(execution_time, process_list):
     start_time = now()
     while(runtime):
 
-        # TODO do operations
-
         print(f'\nIteration #{idx}...\n')
 
         for process in process_list:
             if(debug_mode):
                 print(f'will analyze instances for {process}')
-            procmon.Process.Run_Shell_Command(process)
+            grepped_ps_command = procmon.Utils.make_ps_grep_process(process)
+            procmon.Process.Run_Shell_Command(grepped_ps_command)
 
+        # stop the execution pipeline after the runtime reachers execution time
         if(now() - start_time >= execution_time):
             runtime = False
             break
         idx += 1
         time.sleep(1)
+
+    if(runtime == False and idx):
+        return 1
+    return 0
 
 
 if __name__ == '__main__':
@@ -46,8 +50,13 @@ if __name__ == '__main__':
     # Create the process list and save it to a file
     watch.Create_External_Process_List(
         register.Register.process_list_file_name)
-    process_list = watch.Get_Processes_From_Process_List(
+    PROCESS_LIST = watch.Get_Processes_From_Process_List(
         register.Register.process_list_file_name)
-    print(process_list)
 
     EXECUTION_TIME = 10
+
+    if(PIPELINE == True):
+        PIPELINE_EXECUTION = Execute_Process_Monitor(
+            EXECUTION_TIME, PROCESS_LIST)
+        if(PIPELINE_EXECUTION):
+            reg.Clean_All(reg.register_directory)
