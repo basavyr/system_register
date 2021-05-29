68d28b8d65aa76af4c52d76c35928c774f4beaf2
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-29 21:35:39 +0300

    testing the register class for creating the process list

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from src import pipeline
+from src import register
 
-
-print(pipeline.SayHi())
+x = pipeline.Create_Process_List()
+register.Monitoring.Purge_External_Process_List(
+    register.Register.process_list_file_name)
