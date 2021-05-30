64a0c68aa63dd673e25ed5d9d0f0b9db1a81f35a
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-30 08:20:29 +0300

    add test for file creation

--- test_pipeline.py
+++ test_pipeline.py
@@ -2,10 +2,17 @@
 from src import pipeline
 from src import register
 import os
+import time
 
+processes = pipeline.Create_Process_List()
 
-x = pipeline.Create_Process_List()
+process_list_file = register.Monitoring.Create_Process_Filename(
+    register.Register.process_list_file_name)
 
-# register.Monitoring.Purge_External_Process_List(
-#     register.Register.process_list_file_name)
-print(x)
+proc_file = os.path.isfile(process_list_file)
+
+if(proc_file):
+    print('Success')
+
+if(proc_file):
+    os.remove(process_list_file)
