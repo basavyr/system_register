d813340b1c0854415c1680e9266c8f76661d77ae
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-30 08:47:11 +0300

    create a docker test

--- test_pipeline.py
+++ test_pipeline.py
@@ -4,15 +4,27 @@ from src import register
 import os
 import time
 
-processes = pipeline.Create_Process_List()
 
-process_list_file = register.Monitoring.Create_Process_Filename(
-    register.Register.process_list_file_name)
+def Test_Process_File():
+    processes = pipeline.Create_Process_List()
 
-proc_file = os.path.isfile(process_list_file)
+    process_list_file = register.Monitoring.Create_Process_Filename(
+        register.Register.process_list_file_name)
 
-if(proc_file):
-    print('Success')
+    proc_file = os.path.isfile(process_list_file)
 
-if(proc_file):
-    os.remove(process_list_file)
+    if(proc_file):
+        # print('Success')
+        os.remove(process_list_file)
+        return 1
+
+    return -1
+
+
+if __name__ == '__main__':
+    print('Running process file test...')
+    x = Test_Process_File()
+    if(x == 1):
+        print('Success!')
+    else:
+        print('Test failed.')
