f8c5a1c4ed6de0f77e1ae59396bd9e8be2147bdb
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-30 09:17:42 +0300

    Update test_pipeline.py

--- test_pipeline.py
+++ test_pipeline.py
@@ -6,25 +6,27 @@ import time
 
 
 def Test_Process_File():
-    processes = pipeline.Create_Process_List()
+    pipeline.Create_Process_List()
 
     process_list_file = register.Monitoring.Create_Process_Filename(
         register.Register.process_list_file_name)
 
     proc_file = os.path.isfile(process_list_file)
 
+    fail_status = 1
+
     if(proc_file):
         # print('Success')
         os.remove(process_list_file)
-        return 1
+        fail_status = 0
 
-    return -1
+    if(fail_status):
+        print('Test failed.')
+    else:
+        print('Success!')
 
 
 if __name__ == '__main__':
+    #step1 -> check creation of the process list
     print('Running process file test...')
-    x = Test_Process_File()
-    if(x == 1):
-        print('Success!')
-    else:
-        print('Test failed.')
+    Test_Process_File()
